5f18f332382077c01838a695165f4bfe705e5f6d
Darlan Souza Santos Cruz <100162578+Dansouza911@users.noreply.github.com>
2022-06-27 20:48:02 -0300

    feature(users): created base files

---
+++ src/users/users.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { UserService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UserService) {}
+}
