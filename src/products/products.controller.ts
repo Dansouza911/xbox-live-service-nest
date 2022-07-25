37f8b22da842e10afed42fe7ac22a8b48d090e14
Darlan Souza Santos Cruz <100162578+Dansouza911@users.noreply.github.com>
2022-07-24 21:21:12 -0300

    feature: created the base of the products module, also created the prisma products model

---
+++ src/products/products.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('products')
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Criação de Produtos',
+  })
+  create(@Body() dto: CreateProductDto) {
+    return this.productsService.create(dto);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Listagem de Produtos',
+  })
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'listagem  de um Produtos',
+  })
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualização de Produtos',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
+    return this.productsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({
+    summary: 'Deletar um Produtos',
+  })
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
+  }
+}
