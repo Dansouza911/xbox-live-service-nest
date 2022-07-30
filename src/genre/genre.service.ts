3047492af27b3f629c0e6c967b77f260faf3002b
Darlan Souza Santos Cruz <100162578+Dansouza911@users.noreply.github.com>
2022-07-30 20:34:05 -0300

    feature: id erros validation

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGenreDto } from './dto/create-genre.dto';
 import { UpdateGenreDto } from './dto/update-genre.dto';
@@ -16,15 +20,38 @@ export class GenreService {
     return this.prisma.genre.findMany();
   }
 
-  findOne(id: string): Promise<Genre> {
-    return this.prisma.genre.findUnique({ where: { id } });
+  async verifyIdAndReturnGenre(id: string): Promise<Genre> {
+    const genre: Genre = await this.prisma.genre.findUnique({
+      where: { id },
+    });
+
+    if (!genre) {
+      throw new NotFoundException(`Entrada do '${id}' não encontrada`);
+    }
+    return genre;
   }
 
-  update(id: string, dto: UpdateGenreDto): Promise<Genre> {
+  handleConstrainUniqueError(error: Error): never {
+    const splitedMessage = error.message.split('`');
+    const errorMessage = `Entrada '${
+      splitedMessage[splitedMessage.length - 2]
+    }' não está respeitando a constrant UNIQUE`;
+    throw new UnprocessableEntityException(errorMessage);
+  }
+
+  findOne(id: string): Promise<Genre> {
+    return this.verifyIdAndReturnGenre(id);
+  }
+
+  async update(id: string, dto: UpdateGenreDto): Promise<Genre> {
+    await this.verifyIdAndReturnGenre(id);
+
     return this.prisma.genre.update({ where: { id }, data: dto });
   }
 
-  remove(id: string) {
+  async remove(id: string) {
+    await this.verifyIdAndReturnGenre(id);
+
     return this.prisma.genre.delete({ where: { id } });
   }
 }
